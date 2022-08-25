7960a58df99439d6657b88cc3d51f14baf664aa5
Junior Sousa <jrsousadev@gmail.com>
2022-08-25 18:24:30 -0300

    (main)(server): added routes getAll

--- server/src/shared/routes/eventCalendarRoutes.ts
+++ server/src/shared/routes/eventCalendarRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { CreateEventCalendarController } from "../../modules/EventCalendar/useCases/CreateEventCalendar/CreateEventCalendarController";
+import { GetAllEventCalendarController } from "../../modules/EventCalendar/useCases/GetAllEventCalendar/GetAllEventCalendarController";
 
 const eventCalendarRoutes = Router();
 
 const createEventCalendarController = new CreateEventCalendarController();
 eventCalendarRoutes.post("/", createEventCalendarController.handle);
+
+const getAllEventCalendarController = new GetAllEventCalendarController();
+eventCalendarRoutes.get("/", getAllEventCalendarController.handle);
 
 export default eventCalendarRoutes;
