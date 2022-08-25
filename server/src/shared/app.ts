44127b14eb8305ae09502d2f16151a1a9daece8b
Junior Sousa <jrsousadev@gmail.com>
2022-08-25 18:16:30 -0300

    (main)(server): finished configurate app.ts

--- server/src/shared/app.ts
+++ server/src/shared/app.ts
@@ -1,6 +1,12 @@
+import dotenv from "dotenv";
 import express from "express";
 import routes from "./routes";
 import cors from "cors";
+import "reflect-metadata"
+import mongoose from "mongoose";
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL as string);
 
 const app = express();
 
