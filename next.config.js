3a76e63c02346f1ee7d9e59e4b4759706414fd85
Junior Sousa <jrsousadev@gmail.com>
2022-08-25 12:41:14 -0300

    (main): added dependencies, finish config next.config.js added dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withTM = require("next-transpile-modules")([
+  "@fullcalendar/common",
+  "@babel/preset-react",
+  "@fullcalendar/common",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/interaction",
+  "@fullcalendar/react",
+  "@fullcalendar/timegrid",
+]);
+
+module.exports = withTM({
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+});
