25617b78a67764001244264df543aeedd99a48cf
Junior Sousa <jrsousadev@gmail.com>
2022-08-25 18:09:55 -0300

    (main)(server): added router post

--- server/src/shared/routes/eventCalendarRoutes.ts
+++ server/src/shared/routes/eventCalendarRoutes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { CreateEventCalendarController } from "../../modules/EventCalendar/useCases/CreateEventCalendar/CreateEventCalendarController";
 
 const eventCalendarRoutes = Router();
+
+const createEventCalendarController = new CreateEventCalendarController();
+eventCalendarRoutes.post("/", createEventCalendarController.handle);
 
 export default eventCalendarRoutes;
