978d75ba5dce72e56152f632e517c6b99612ab51
Junior Sousa <jrsousadev@gmail.com>
2022-08-25 13:57:36 -0300

    (main): create routes

---
+++ src/services/eventCalendarRoutes.ts
@@ -0,0 +1,5 @@
+export const CREATE_EVENT_CALENDAR = 'api/eventCalendar';
+export const GET_EVENT_CALENDAR = (id: string) => `api/eventCalendar/${id}`;
+export const GET_ALL_EVENTS_CALENDAR_BY_USER = 'api/eventCalendar/user';
+export const UPDATE_EVENT_CALENDAR = 'api/eventCalendar';
+export const DELETE_EVENT_CALENDAR = (id: string) => `api/eventCalendar/${id}`;
