7f1c24845f1c55342d6e1ffd361ce2cc7e0ccd0f
Junior Sousa <jrsousadev@gmail.com>
2022-08-25 22:21:46 -0300

    fix folders

--- server/src/shared/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import eventCalendarRoutes from "./eventCalendarRoutes";
-
-const routes = Router();
-
-routes.use("/api/eventCalendar", eventCalendarRoutes);
-
-export default routes;
--- server/src/shared/server.ts
+++
@@ -1,5 +0,0 @@
-import app from "./app";
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`🚀 Server started on port ${PORT}!`));
